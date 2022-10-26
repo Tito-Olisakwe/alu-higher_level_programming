0a3dbe12b06b33415127362c452f50174e68799e
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-10-26 12:28:05 +0200

    commit

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123, -1):
+for i in range(122, 96, -1):
     if i % 2 == 0:
-        print("{}".format(chr(i)), end="")
-    else:
-        print("{}".format(chr(ord(i)-32)), end="")
+        i -= 32
+    print("{}".format(chr(i)), end="")
