bbaffe271799c19ea98eda208b98c5a1afae260e
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-10-26 11:41:14 +0200

    commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upper_string = ''
     for i in str:
         if ord(i) > 96 and ord(i) < 123:
-            upper_string = chr(ord(i)+32)
-        else:
-            upper_string = chr(ord(1))
-        print('{}'.format(upper_string), end='')
+            i = chr(ord(i)+32)
+        print('{}'.format(i), end='')
+    print('')
