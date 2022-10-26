55c01d1ffbcbde99a76fba6277f66175a0c30b29
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-10-26 13:50:06 +0200

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,8 @@ from sys import argv
 if __name__ == "__main__":
     lenght = len(argv) - 1
     if lenght == 1:
-        print('1 argument:\n1: {}'.format(argv[0]))
+        print('1 argument:')
+        print('1: {}'.format(argv[1]))
     elif lenght == 0:
         print('0 arguments.')
     else:
