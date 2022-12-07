e5c3405eba531cb962814cf3072b0dc2e0668177
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-12-07 03:48:40 +0200

    commit

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,11 +8,18 @@ def matrix_divided(matrix, div):
     """
         Write a function that divides all elements of a matrix.
         Prototype: def matrix_divided(matrix, div):
-        matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception with the message matrix must be a matrix (list of lists) of integers/floats
-        Each row of the matrix must be of the same size, otherwise raise a TypeError exception with the message Each row of the matrix must have the same size
-        div must be a number (integer or float), otherwise raise a TypeError exception with the message div must be a number
-        div cannot be equal to 0, otherwise raise a ZeroDivisionError exception with the message division by zero
-        All elements of the matrix should be divided by div, rounded to 2 decimal places
+        matrix must be a list of lists of integers or floats,
+        otherwise raise a TypeError exception with the message
+        matrix must be a matrix (list of lists) of integers/floats
+        Each row of the matrix must be of the same size, otherwise
+        raise a TypeError exception with the message Each row of the
+        matrix must have the same size
+        div must be a number (integer or float), otherwise raise a
+        TypeError exception with the message div must be a number
+        div cannot be equal to 0, otherwise raise a ZeroDivisionError
+        exception with the message division by zero
+        All elements of the matrix should be divided by div, 
+        rounded to 2 decimal places
         Returns a new matrix
 
     """
