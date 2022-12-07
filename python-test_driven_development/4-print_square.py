ba20e38551e1746d2e4d3bbbcb5684bdb220f33a
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-12-07 03:58:01 +0200

    commit

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,9 +9,12 @@ def print_square(size):
         Write a function that prints a square with the character #.
         Prototype: def print_square(size):
         size is the size length of the square
-        size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-        if size is less than 0, raise a ValueError exception with the message size must be >= 0
-        if size is a float and is less than 0, raise a TypeError exception with the message size must be an integer
+        size must be an integer, otherwise raise a TypeError exception
+        with the message size must be an integer
+        if size is less than 0, raise a ValueError exception with the
+        message size must be >= 0
+        if size is a float and is less than 0, raise a TypeError
+        exception with the message size must be an integer
     """
     if type(size) != int:
         raise TypeError("size must be an integer")
