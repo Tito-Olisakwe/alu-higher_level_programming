fd201febca34115fd27c42ed0d304775fb296c89
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-12-07 03:53:42 +0200

    commit

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,7 +8,9 @@ def say_my_name(first_name, last_name=""):
     """
         Write a function that prints My name is <first name> <last name>
         Prototype: def say_my_name(first_name, last_name=""):
-        first_name and last_name must be strings otherwise, raise a TypeError exception with the message first_name must be a string or last_name must be a string
+        first_name and last_name must be strings otherwise, raise a
+        TypeError exception with the message first_name must be a string
+        or last_name must be a string
 
     """
     if type(first_name) != str:
