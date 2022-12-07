3037f8b12b2beee6a26311c912df663e9d435993
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-12-07 04:39:24 +0200

    commit

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,9 +7,11 @@ import numpy as NumPy
 
 def lazy_matrix_mul(m_a, m_b):
     """
-        Write a function that multiplies 2 matrices by using the module NumPy
+        Write a function that multiplies 2 matrices by using 
+        the module NumPy
         Prototype: def lazy_matrix_mul(m_a, m_b):
-        Test cases should be the same as 100-matrix_mul but with new exception type/message
+        Test cases should be the same as 100-matrix_mul but 
+        with new exception type/message
     """
 
     return (NumPy.matmul(m_a, m_b))
