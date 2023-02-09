3622578d1826ca221c9eeca2cf94e90ff96f82cc
Tito-Olisakwe <t.olisakwe@alustudent.com>
2023-02-09 07:50:24 +0200

    commit

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+let data;
+
+request.get(url, function (err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    data = JSON.parse(res.body).results;
+    data.forEach((obj) => {
+      obj.characters.forEach((character) => {
+        if (character.includes('/18/')) count++;
+      });
+    });
+  }
+  console.log(count);
+});
